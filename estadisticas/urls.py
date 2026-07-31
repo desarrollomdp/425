642ed050f67ed0fc898ec81c9874fd147ae969eb
Ezequiel <desarrollomdp00@gmail.com>
2026-07-31 19:47:48 -0300

    Actualizacion (excluyendo base de datos)

--- estadisticas/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import estadisticas_view
-
-urlpatterns = [
-    path('estadisticas/', estadisticas_view, name='estadisticas'),
-]
--- formulario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import inscripcion_view, gracias_view, inicio_views
-from django.conf.urls.static import static
-from django.conf import settings
-urlpatterns = [
-    path('votacion/', inscripcion_view, name='votacion'),
-    path('gracias/', gracias_view, name='gracias'),
-    path('', inicio_views, name='inicio'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
